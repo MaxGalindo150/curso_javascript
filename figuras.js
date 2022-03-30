79ab3507cc84c7e0c6d237eff6c9cda2753d090f
MaxGalindo150 <maximilianogalindo7@gmail.com>
2022-03-30 15:58:01 -0600

    I Introduced functions

--- figuras.js
+++ figuras.js
@@ -1,55 +1,55 @@
 //Cuadrado
 console.group("Cuadrados");
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadrado mide: " + perimetroCuadrado + " cm");
+function perimetroCuadrado(lado){
+    return lado*4;
+}
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El area del cuadrado mide: " + areaCuadrado + " cm2")
+function areaCuadrado(lado){
+    return lado*lado
+}
 console.groupEnd();
+
 //Triangulo
 console.group("Triangulos")
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const ladoBase = 4;
-const alturaTriangulo = 5.5;
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoBase;
-const areaTriangulo = (ladoBase*alturaTriangulo)/2;
+function perimetroTriangulo(lado1, lado2, lado3){
+    return lado1 + lado3 + lado3
+}
 
-console.log(
-    "Los lados del triangulo miden: " 
-    + ladoTriangulo1 + " cm, " 
-    + ladoTriangulo2 + " cm y " 
-    + ladoBase + " cm"
-);
-
-console.log("La altura del triangulo es de: " + alturaTriangulo + " cm");
-console.log("El perimetro del triangulo es de: " + perimetroTriangulo + " cm");
-console.log("El area del triangulo es de: " + areaCuadrado + " cm2");
+function areaTriangulo(base, altura){
+    return (base*altura)/2
+}
 
 console.groupEnd();
 
 //Circulo
 console.group("Circulo");
-const radio = 4;
 const pi = Math.PI
-const perimetroCirculo = 2*pi*radio
-const areaCirculo = pi*(radio*radio)
 
-console.log("El radio del circulo es: " + radio + " cm")
-console.log("El perimetro del circulo es: " + perimetroCirculo + " cm")
-console.log("El area del circulo es: " + areaCirculo + " cm")
+function perimetroCirculo(radio){
+    return 2*pi*radio
+}
 
+function areaCirculo(radio){
+    return pi*(radio*radio)
+}
 
 console.groupEnd();
 
-//console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm")
+//Interectuamos con html
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
 
-//const perimetroCuadrado = ladoCuadrado * 4;
-//console.log("El perimetro del cuadrado mide: " + perimetroCuadrado + " cm")
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
 
-//const areaCuadrado = ladoCuadrado * ladoCuadrado
-//console.log("El area del cuadrado mide: " + areaCuadrado + " cm2")
+function calcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
 
